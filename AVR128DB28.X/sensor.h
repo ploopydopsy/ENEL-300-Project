739b777e15a709debf93c20f4b676953559ae78f
ploopydopsy <william.adebiyi@gmail.com>
2025-03-16 18:11:26 -0600

    setup

--- AVR128DB28.X/sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSOR_H
-#define SENSOR_H
-
-#include <stdint.h>
-
-void sensor_init(void);
-uint16_t measure_distance_cm(void);
-
-#endif
--- AVR128DB28.X/stdio.h
+++
@@ -1,9 +0,0 @@
-#ifndef STDIO_H
-#define STDIO_H
-
-#include "stdarg.h"
-
-int sprintf(char *str, const char *format, ...);
-int vsprintf(char *str, const char *format, va_list ap);
-
-#endif // STDIO_H
